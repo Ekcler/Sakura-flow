4c5ee4df2a35f95f60488d185def70cdd2a9c007
Ekcler <arbyzbebrovich.12@gmail.com>
2026-04-13 17:36:49 +0300

    Fix build script: include proxy module

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build executable with PyInstaller on Windows
-pyinstaller --onedir --noconfirm --noconsole --name SakuraFlow --manifest manifest.xml --add-data "icons;icons" --add-data "zapret;zapret" --add-data "src/tg_ws_proxy.py;." --icon=icons/moonstone.ico --version-file=version.py src/main.py
+pyinstaller --onedir --noconfirm --noconsole --name SakuraFlow --manifest manifest.xml --add-data "icons;icons" --add-data "zapret;zapret" --add-data "src/service.py;src" --add-data "src;proxy" --add-data "src/tg_ws_proxy.py;." --icon=icons/moonstone.ico --version-file=version.py src/main.py
